bf9ed6e32460cae60b21e72c99ae061c287e1e1f
crawlic-stud <nikitosik0726@gmail.com>
2023-12-09 14:41:25 +0300

    added admin

---
+++ admin/main.py
@@ -0,0 +1,37 @@
+import os
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.routing import Route
+from starlette_admin.contrib.mongoengine import Admin as StarletteAdmin, ModelView
+from mongoengine import connect, disconnect
+from dotenv import load_dotenv
+
+from models import User, Admin, Product, Event
+
+
+load_dotenv()
+MONGO_URL = f'mongodb://{os.environ["MONGO_USER"]}:{os.environ["MONGO_PASSWORD"]}@{os.environ["MONGO_HOST"]}:{os.environ["MONGO_PORT"]}'
+MONGO_URL = os.environ["DATABASE_ADMIN_URL"]
+print(MONGO_URL)
+
+app = Starlette(
+    routes=[
+        Route(
+            "/",
+            lambda r: HTMLResponse('<a href="/admin/">Click me to get to Admin!</a>'),
+        )
+    ],
+    on_startup=[lambda: connect(host=MONGO_URL)],
+    on_shutdown=[lambda: disconnect()],
+)
+
+# Create admin
+admin = StarletteAdmin(title="Example: MongoEngine")
+
+admin.add_view(ModelView(User, icon="fa fa-users"))
+admin.add_view(ModelView(Admin, icon="fa fa-users"))
+admin.add_view(ModelView(Product, icon="fa fa-list"))
+admin.add_view(ModelView(Event, icon="fa fa-list"))
+
+# Mount admin to app
+admin.mount_to(app)
---
+++ admin/models.py
@@ -0,0 +1,30 @@
+import mongoengine as db
+
+
+class Product(db.Document):
+    meta = {"collection": "products"}
+    name = db.StringField(max_length=256)
+    description = db.StringField()
+    image_id = db.StringField(max_length=256)
+    price = db.IntField()
+
+
+class Event(db.Document):
+    meta = {"collection": "events"}
+    name = db.StringField(max_length=256)
+    description = db.StringField()
+    image_id = db.StringField(max_length=256)
+    date = db.DateTimeField(required=False)
+
+
+class User(db.Document):
+    meta = {"collection": "users"}
+    user_id = db.IntField()
+    username = db.StringField(max_length=256)
+    first_name = db.StringField(max_length=256)
+    last_name = db.StringField(max_length=256)
+
+
+class Admin(db.Document):
+    meta = {"collection": "admins"}
+    user_id = db.IntField()
